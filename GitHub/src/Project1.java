c9763a94ba38ed44c45ddcdc9e0e1bc2017d2efe
Admin <Admin@DESKTOP-6I3KOB4>
2024-11-09 23:35:13 +0530

    First_Commit

---
+++ GitHub/src/Project1.java
@@ -0,0 +1,10 @@
+
+public class Project1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To GitHub!");
+		System.out.println("Projects Will be uploaded from Eclipse");
+
+	}
+
+}
